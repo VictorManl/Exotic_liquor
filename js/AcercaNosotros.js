7de456c459b2946a0dace35918955ea781f878c2
Victor manuel <victormel2003@gmail.com>
2021-12-11 15:54:49 -0500

    Responsive

--- js/AcercaNosotros.js
+++ js/AcercaNosotros.js
@@ -1,8 +1,8 @@
 if(screen.width < 1030){
 	document.write('<link rel="stylesheet" href="Responisve/1030/AcercaNosotros.css"></link>');
 }
-else if(screen.width < 1280){
-	document.write('<link rel="stylesheet" href=""></link>');
+else if(screen.width < 1290){
+	document.write('<link rel="stylesheet" href="Responisve/1280/AcercaNosotros></link>');
 }
 else if(screen.width < 1380){
 	document.write('<link rel="stylesheet" href="Responisve/1380/AcercaNosotros.css"></link>'); 
